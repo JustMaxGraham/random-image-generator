df7aa2034132785cc67d4bce27256486b0f880ad
MaxHuw <m.huw.graham@gmail.com>
2021-10-21 11:58:01 -0400

    Basic image generation

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-import mergeImages from 'merge-images';
+import mergeImages from 'merge-images-v2';
 import * as fs from 'fs';
 import Canvas from 'canvas';
-
-const { Image } = Canvas
-
 const imagesPath = './images'
 
-async function printFolders(imagePath) {
+async function printFolders(imagesPath) {
   try {
     await fs.readdir(imagesPath, (err, files) => {
       files.forEach(file => {
@@ -18,4 +15,21 @@ async function printFolders(imagePath) {
   }
 }
 
-printFolders(imagesPath);
+// Test to merge images
+mergeImages([
+  './images/1_Backgrounds/Blue.png', 
+  './images/2_Mugs/Mug_Red.png', 
+  './images/3_Shading/Shading.png',
+  './images/4_Image/Image Cat.png'], {
+  Canvas: Canvas,
+})
+  .then(b64 => {
+    let data = b64.replace(/^data:image\/\w+;base64,/, "");
+    let buf = Buffer.from(data, 'base64');
+    fs.writeFile('./images/Results/test.png', buf, function(err) {
+      if (err) {
+        console.log(err)
+      }
+      console.log("Image Created")
+    })
+  });
