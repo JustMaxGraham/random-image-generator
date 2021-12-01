e968193e60487daa0c4cb1bfedf5a03ceceb3622
MaxHuw <m.huw.graham@gmail.com>
2021-12-01 14:44:44 -0500

    Metadata for Solana

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import mergeImages from "merge-images-v2";
 import * as fs from "fs";
 import Canvas from "canvas";
 import util from "util";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const imagesPath = "./images";
 const dnaArray = [];
@@ -44,7 +47,7 @@ function getRandomInt(max) {
 
 function getImageDNA(fileName) {
   //console.log("Filename for DNA: ", fileName);
-  let dnaStrand = fileName.split("_")[1].split(".")[0];
+  let dnaStrand = fileName.split("_")[0];
   //console.log("Strand ", dnaStrand);
   return dnaStrand;
 }
@@ -69,68 +72,107 @@ function buildImages(num) {
   getFolders();
   getFiles();
 
-  if (uniqueAttempt < 10) {
-    for (let i = 0; i < num; i++) {
-      let imgObj = buildImgObject();
+  for (let i = 0; i < num; i++) {
+    console.log("i & unique", i, uniqueAttempt);
+    let imgObj = buildImgObject();
 
-      if (!dnaArray.includes(imgObj.imageDNA)) {
-        uniqueAttempt = 0;
-        dnaArray.push(imgObj.imageDNA);
-        // console.log(dnaArray);
+    if (!dnaArray.includes(imgObj.imageDNA)) {
+      uniqueAttempt = 0;
+      dnaArray.push(imgObj.imageDNA);
+      // console.log(dnaArray);
 
-        let metaData = {
-          dna: "",
-          image: "url",
-          tokenId: i,
-          name: `Crypto Mug ${i}`,
-          attributes: [],
+      // OLD META
+
+      // let metaData = {
+      //   dna: "",
+      //   image: "url",
+      //   tokenId: i,
+      //   name: `Crypto Mug ${i}`,
+      //   attributes: [],
+      // };
+
+      let metaData = {
+        name: `Crypto Mug Test ${i}`,
+        symbol: "",
+        description: "A test collection of randomly generated pixel mugs.",
+        seller_fee_basis_points: 500,
+        image: "image.png",
+        attributes: [],
+        collection: {
+          name: "Crypto Mug Test",
+          family: "JustMax NFTs",
+        },
+        properties: {
+          files: [
+            {
+              uri: "image.png",
+              type: "image/png",
+            },
+          ],
+          category: "image",
+          creators: [
+            {
+              address: `${process.env.MY_WALLET}`,
+              share: 100,
+            },
+          ],
+        },
+      };
+
+      let attArr = [];
+
+      for (let x = 0; x < imgObj.imageArray.length; x++) {
+        let string = imgObj.imageArray[x].split("/").slice(2);
+        // console.log(string);
+        let obj = {
+          trait_type: string[0].split("_")[1],
+          value: string[1].split("_")[1],
         };
-
-        metaData.dna = imgObj.imageDNA;
-
-        let attArr = [];
-
-        for (let x = 0; x < imgObj.imageArray.length; x++) {
-          let string = imgObj.imageArray[x].split("/").slice(2);
-          let obj = {
-            trait_type: string[0],
-            value: string[1],
-          };
-          metaData.attributes.push(obj);
-          // console.log("object: ", obj);
-        }
-
-        metaData.attributes.push(attArr);
-
-        jsonData.push(metaData);
-
-        mergeImages(imgObj.imageArray, {
-          Canvas: Canvas,
-        }).then((b64) => {
-          let data = b64.replace(/^data:image\/\w+;base64,/, "");
-          let buf = Buffer.from(data, "base64");
-          fs.writeFile(
-            `./images/Results/Crypto_Mug_${i}.png`,
-            buf,
-            function (err) {
-              if (err) {
-                console.log(err);
-              }
-            }
-          );
-        });
-
-        console.log(`Crypto Mug #${i} Created!`);
-      } else {
-        uniqueAttempt += 1;
-        console.log("Image not unique, trying again");
+        metaData.attributes.push(obj);
+        // console.log("object: ", obj);
       }
-      // console.log(imgArr);
+
+      // metaData.attributes.push(attArr);
+
+      jsonData.push(metaData);
+      // Write JSON file
+      let dictstring = JSON.stringify(metaData);
+      // console.log(dictstring);
+      fs.writeFile(`./images/Results/${i}.json`, dictstring, (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(`${i}.json CREATED`);
+      });
+
+      //Make the image
+      mergeImages(imgObj.imageArray, {
+        Canvas: Canvas,
+      }).then((b64) => {
+        let data = b64.replace(/^data:image\/\w+;base64,/, "");
+        let buf = Buffer.from(data, "base64");
+        fs.writeFile(`./images/Results/${i}.png`, buf, function (err) {
+          if (err) {
+            console.log(err);
+          }
+          console.log(`${i}.png CREATED`);
+        });
+      });
+
+      console.log(`Crypto Mug #${i} Created!`);
+    } else if (uniqueAttempt === 10) {
+      console.log("Unique Attempt ", uniqueAttempt);
+      console.log("Could not build a unique image too many times, quitting...");
+      break;
+    } else {
+      i -= 1;
+      uniqueAttempt += 1;
+      console.log("Image not unique, trying again", uniqueAttempt);
     }
-  } else {
-    console.log("Failed to create a unique image too many times, quitting.");
+    // console.log(imgArr);
   }
 }
 
-buildImages(1);
-console.log("Resutls: ", util.inspect(jsonData, false, null, true));
+buildImages(4);
+// console.log("Resutls: ", util.inspect(jsonData, false, null, true));
+// console.log("Number of unique images.", jsonData.length);
