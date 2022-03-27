6a481f05150b33e40182b4caf74eb5a49cead6b2
MaxHuw <m.huw.graham@gmail.com>
2022-03-27 17:47:41 -0400

    promises and filter out DS files

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import mergeImages from "merge-images-v2";
 import * as fs from "fs";
 import Canvas from "canvas";
 import dotenv from "dotenv";
+import { resolve } from "path";
 
+const fsPromises = fs.promises;
 dotenv.config();
 
 const imagesPath = "./images";
-const dnaArray = [];
+let dnaArray = [];
+let addExtra = 0;
 let uniqueAttempt = 0;
 
 let folders = []; // ["folder1", "folder2", "folder3"]
@@ -25,7 +28,8 @@ function getFolders() {
       }
     });
   } catch (err) {
-    console.log(err);
+    console.log("Error in get Folders:");
+    // console.log(err);
   }
 }
 
@@ -34,7 +38,11 @@ function getFolders() {
 function getFiles() {
   let contents = [];
   folders.forEach((folder) => {
-    attributes.push(fs.readdirSync(`${folder}/`));
+    // console.log(fs.readdirSync(`${folder}/`));
+
+    attributes.push(
+      fs.readdirSync(`${folder}/`).filter((item) => !/.DS/gm.test(item))
+    );
   });
 }
 
@@ -56,26 +64,88 @@ function buildImgObject() {
   };
   folders.forEach((folder, index) => {
     // console.log("Folder", folder.split("/")[2]);
-    let applyExtra = getRandomInt(10);
+    let skipExtraRoll = getRandomInt(10);
     // console.log("Random Extra%:", getRandomInt(10));
     // console.log(
     //   "Add extra?: ",
     //   folder.split("/")[2] === "5_Extra" && getRandomInt(10) <= 2
     // );
-    if (folder.split("/")[2] === "5_Extra" && applyExtra >= 8) {
-      console.log("Extra change?:", applyExtra);
-      return;
-    }
+    // let applyBoolean = folder.split("/")[2] === "6_Extra" && skipExtraRoll >= 8;
+    // console.log("Att type/folder: ", folder.split("/")[2]);
+    // if (folder.split("/")[2] === "8_Extra" && skipExtraRoll <= 8) {
+    //   // console.log("extra folder: ", folder.split("/")[2], applyExtra);
+    //   // console.log(applyBoolean);
+    //   // console.log("Extra change?:", applyExtra);
+    //   // addExtra++;
+    //   return;
+    // }
     let currentFile = attributes[index][getRandomInt(attributes[index].length)];
+
+    // switch (folder.split("/")[2]) {
+    //   case "8_Extra":
+    //     // code block
+    //     if (skipExtraRoll <= 8) {
+    //       return;
+    //     }
+    //     break;
+    //   // case y:
+    //   //   // code block
+    //   //   break;
+    //   // default:
+    //   // code block
+
+    //   // console.log(currentFile);
+    // }
     imgageObject.imageDNA += getImageDNA(currentFile);
+    // console.log(`${folder}${currentFile}`);
     imgageObject.imageArray.push(`${folder}${currentFile}`);
   });
 
   return imgageObject;
 }
 
+async function generateImage(imageArray, index) {
+  new Promise((resolve, reject) => {
+    try {
+      mergeImages(imageArray, {
+        Canvas: Canvas,
+      }).then((b64) => {
+        let data = b64.replace(/^data:image\/\w+;base64,/, "");
+        let buf = Buffer.from(data, "base64");
+        fs.writeFile(`./images/Results/${index}.png`, buf, (err) => {
+          if (err) {
+            console.log("Error in merge Images.");
+            // console.log(err);
+          }
+        });
+      });
+    } catch (ex) {
+      reject(ex);
+      console.log("Issue generating image.");
+    }
+  });
+}
+
+function writeJSON(i, metaData) {
+  console.log(i, metaData);
+  return new Promise((resolve, reject) => {
+    try {
+      let dictstring = JSON.stringify(metaData);
+      fs.writeFile(`./images/Results/${i}.json`, dictstring, (err, results) => {
+        if (err) {
+          console.log("Errro in write JSON file");
+          // console.log(err);
+        }
+        console.log(`${i}.json CREATED`);
+      });
+    } catch (ex) {
+      reject(ex);
+    }
+  });
+}
+
 // Test to merge images
-function buildImages(num) {
+async function buildImages(num) {
   getFolders();
   getFiles();
 
@@ -87,7 +157,7 @@ function buildImages(num) {
       uniqueAttempt = 0;
       dnaArray.push(imgObj.imageDNA);
 
-      let metaData = {
+      var metaData = {
         name: `Crypto Mug Test ${i}`,
         symbol: "",
         description: "A test collection of randomly generated pixel mugs.",
@@ -122,34 +192,35 @@ function buildImages(num) {
         let obj = {
           trait_type: string[0].split("_")[1],
           value: string[1].split("_")[1].split(".")[0],
+          // value: string[1].split("_")[1],
         };
         metaData.attributes.push(obj);
       }
 
       // Write JSON file
-      let dictstring = JSON.stringify(metaData);
-      fs.writeFile(`./images/Results/${i}.json`, dictstring, (err, results) => {
-        if (err) {
-          console.log(err);
-        }
-        console.log(`${i}.json CREATED`);
-      });
 
       //Make the image
-      mergeImages(imgObj.imageArray, {
-        Canvas: Canvas,
-      }).then((b64) => {
-        let data = b64.replace(/^data:image\/\w+;base64,/, "");
-        let buf = Buffer.from(data, "base64");
-        fs.writeFile(`./images/Results/${i}.png`, buf, function (err) {
-          if (err) {
-            console.log(err);
-          }
-          console.log(`${i}.png CREATED`);
-        });
-      });
-
-      console.log(`Crypto Mug #${i} Created!`);
+      try {
+        await generateImage(imgObj.imageArray, i);
+        console.log(`${i}.png CREATED`);
+        // await writeJSON(i, metaData);
+      } catch (error) {
+        console.log("error in try catch: ", error);
+      }
+      console.log("next loop: ", i);
+      // mergeImages(imgObj.imageArray, {
+      //   Canvas: Canvas,
+      // }).then((b64) => {
+      //   let data = b64.replace(/^data:image\/\w+;base64,/, "");
+      //   let buf = Buffer.from(data, "base64");
+      //   fs.writeFile(`./images/Results/${i}.png`, buf, (err) => {
+      //     if (err) {
+      //       console.log("Error in merge Images.");
+      //       // console.log(err);
+      //     }
+      //     console.log(`${i}.png CREATED`);
+      //   });
+      // });
     } else if (uniqueAttempt === 10) {
       //Make sure we don't try to make unique images too many times.
       console.log("Unique Attempt ", uniqueAttempt);
@@ -161,6 +232,7 @@ function buildImages(num) {
       console.log("Image not unique, trying again", uniqueAttempt);
     }
   }
+  // console.log("Number of images with extras? :", addExtra);
 }
 
-buildImages(10);
+buildImages(20);
